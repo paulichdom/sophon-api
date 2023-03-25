bc199bc805bdbbaa21ed5ba57709f66ed28f786c
Domagoj <paulic.domagoj@gmail.com>
2023-03-25 06:45:09 +0100

    setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,17 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { join } from 'path';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       include: [CatsModule],
       driver: ApolloDriver,
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
+  console.log(`Application is running on ${await app.getUrl()}`);
 }
 bootstrap();
