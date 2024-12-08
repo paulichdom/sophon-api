291524aa1a6df161b3211823c5703d38203a22b3
Domagoj <paulic.domagoj@gmail.com>
2024-12-08 10:40:31 +0100

    update import statement

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieSession from 'cookie-session';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
