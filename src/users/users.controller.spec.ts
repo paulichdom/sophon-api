426eb33ec0b363a26f85018980029b9364057744
Domagoj <paulic.domagoj@gmail.com>
2024-12-20 08:55:24 +0100

    test user service 109

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth/auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -55,5 +56,22 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers(emailMock);
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual(emailMock);
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    usersServiceMock.findOne = () => null;
+
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
