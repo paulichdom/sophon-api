af0e9787847e8e6c073be1bd563f8675c45f74b4
paulichdom <paulic.domagoj@gmail.com>
2024-12-29 20:22:17 +0100

    apply dotenv for config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import {APP_PIPE} from '@nestjs/core'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {ConfigModule, ConfigService} from '@nestjs/config'
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { ArticlesModule } from './articles/articles.module';
@@ -12,12 +12,27 @@ const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Article]
+        }
+      }
+    }),
+    /* TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [User, Article],
       synchronize: true,
-    }),
+    }), */
     UsersModule,
     ArticlesModule,
   ],
