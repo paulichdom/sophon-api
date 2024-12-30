2a8c23b0c9b306fa5248b4b051ea8c94723ba34c
paulichdom <paulic.domagoj@gmail.com>
2024-12-30 15:35:38 +0100

    add properties to report

--- src/reports/entities/report.entity.ts
+++ src/reports/entities/report.entity.ts
@@ -1,1 +1,25 @@
-export class Report {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+   @Column()
+   year: number;
+
+   @Column()
+   lng: number;
+
+   @Column()
+   lat: number;
+
+   @Column()
+   mileage: number;
+}
