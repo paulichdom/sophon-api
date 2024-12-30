8554c55bffaf6ea0e6c8bc89bc2e6747f8b08d1d
paulichdom <paulic.domagoj@gmail.com>
2024-12-30 15:40:11 +0100

    add create report dto

--- src/reports/dto/create-report.dto.ts
+++ src/reports/dto/create-report.dto.ts
@@ -1,1 +1,15 @@
-export class CreateReportDto {}
+export class CreateReportDto {
+  make: string;
+
+  model: string;
+
+  year: number;
+
+  mileage: number;
+
+  lng: number;
+
+  lat: number;
+
+  price: number;
+}
