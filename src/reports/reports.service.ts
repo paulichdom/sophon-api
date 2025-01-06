267f653e3bd9ed2c8c244de35f8b0391b5034771
paulichdom <paulic.domagoj@gmail.com>
2025-01-06 21:25:35 +0100

    query for the estimate 151

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -12,11 +12,18 @@ import { GetEstimateDto } from './dto/get-estimate';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  createEstimate(estimateDto: GetEstimateDto) {
+  createEstimate({make, model, lng, lat, year, mileage}: GetEstimateDto) {
     return this.repo.createQueryBuilder()
-    .select('*')
-    .where('make = :make', {make: estimateDto.make})
-    .getRawMany()
+    .select('AVG(price)', 'price')
+    .where('make = :make', {make})
+    .andWhere('model = :model', {model})
+    .andWhere('lng = :lng BETEWEEN -5 AND 5', {lng})
+    .andWhere('lat = :lat BETEWEEN -5 AND 5', {lat})
+    .andWhere('year = :year BETEWEEN -3 AND 3', {year})
+    .orderBy('ABS(mileage = :mileage)', 'DESC')
+    .setParameters({mileage})
+    .limit(3)
+    .getRawOne()
   }
 
   create(createReportDto: CreateReportDto, user: User) {
