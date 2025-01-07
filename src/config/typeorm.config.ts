062106ce46dd4feae2d202a61dd714c365dc3639
paulichdom <paulic.domagoj@gmail.com>
2025-01-07 22:15:31 +0100

    setup prod db, refactor config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
- 
+
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
- 
-  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    return {
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const isTest = this.configService.get('NODE_ENV') === 'test';
+    const isDev = this.configService.get('NODE_ENV') === 'development';
+    const isProd = this.configService.get('NODE_ENV') === 'production';
+
+    const baseConfig: TypeOrmModuleOptions = {
       type: 'sqlite',
-      synchronize: process.env.NODE_ENV === 'test' ? true : false,
-      database: this.configService.get<string>('DB_NAME'),
+      database: this.configService.get('DB_NAME'),
       autoLoadEntities: true,
-      migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-      keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
+      synchronize: isTest || isDev,
+      migrationsRun: isTest,
+      keepConnectionAlive: isTest,
+    };
+
+    if (!isProd) {
+      return baseConfig;
+    }
+
+    return {
+      ...baseConfig,
+      type: 'postgres',
+      url: this.configService.get('DATABASE_URL'),
     };
   }
 }
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
  
 export const appDataSource = new DataSource({
   type: 'sqlite',
-  database: 'db.sqlite',
+  database: 'dev-db.sqlite',
   entities: ['**/*.entity*{.js,.ts}'],
   migrations: [__dirname + '/migrations/*{.js,.ts}'],
 } as DataSourceOptions);
