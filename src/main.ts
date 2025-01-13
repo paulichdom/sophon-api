f203f54b5cde5af9e91f1f72dc111d8911fe162e
paulichdom <paulic.domagoj@gmail.com>
2025-01-13 19:25:52 +0100

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sophon Api')
+    .setDescription('RealWorld custom API for social blogging site (i.e. a Medium.com clone)')
+    .setVersion('1.0')
+    .addTag('sophon-api')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory)
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
