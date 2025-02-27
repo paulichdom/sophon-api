893df5c65336a70e7469b0814e01a4df3c8b9a62
paulichdom <paulic.domagoj@gmail.com>
2025-02-27 08:54:24 +0100

    update tag entity and dto, clean up model

---
+++ src/models/tag/dto/tag.dto.ts
@@ -0,0 +1,8 @@
+export class TagData {
+  id: number;
+  name: string;
+}
+
+export class TagDto {
+  tags: string[]
+}
--- src/models/tag/entities/tag.entity.ts
+++ src/models/tag/entities/tag.entity.ts
@@ -1,1 +1,8 @@
-export class Tag {}
+import { BaseEntity } from "src/models/shared/base.entity"
+import { Column, Entity } from "typeorm"
+
+@Entity({name: 'tag'})
+export class Tag extends BaseEntity {s
+  @Column({ unique: true })
+  name: string
+}
